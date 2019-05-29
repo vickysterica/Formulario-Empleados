f2eac01427176c05ae9c3c9ca3f91c91218b92ff
vickysterica <victoriaromerocoy@gmail.com>
2019-05-29 23:05:21 +0200

    creación del formulario

--- Formulario_Emple/inserta_emple.php
+++ Formulario_Emple/inserta_emple.php
@@ -7,15 +7,28 @@
     <h2> Formulario de Inserción de empleados </h2>
     
     <form action="inserta_emple.php" method="POST">
-        EMP_NO
-        APELLIDO
-        OFICIO
-        DIR
-        SALARIO
-        COMISION
-        DEPT_NO
-
-        ESTE COMENTARIO ES NUEVO  ****
+        <p>Introduce un número de empleado :</p>
+            <input type="text" name="dept_no" id="emp_no"/>
+        <p>Introduce un apellido :</p>
+            <input type="text" name="ape" id="ape"/>
+        <p>Selecciona un oficio de la lista :</p>
+            <select name="oficio" id="oficio">
+                <option value="empleado">Empleado</option>
+                <option value="director">Director</option>
+                <option value="vendedor">Vendedor</option>
+                <option value="analista">Analista</option>
+                <option value="presidente">Presidente</option>
+            </select>
+        <p>Selecciona un número de Director :</p>
+            Director Jiménez <input type="radio" value="7566"> 
+            Director Negro <input type="radio" value="7698">
+            Director Cerezo <input type="radio" value="7782">
+        <p>Introduce un salario :</p>
+            <input type="text" name="salario" id="salario">
+        <p>Introduce una comisión :</p>
+            <input type="text" name="comision" id="comision">
+        <p>Introduce el Número de departamento</p>
+  
         
     </form>
 </body>
